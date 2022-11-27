cf62802332afe3ef6fb9b075f191da4fb8911c2c
Damian Martinez <CDMARTINEZ@trabajo.gob.ar>
2022-11-26 23:27:13 -0300

    subida

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <div id="app">
+      <HeaderComp />
+    </div>
+  </section>
+</template>
+
+<script>
+import HeaderComp from "./components/Header.vue";
+export default {
+  name: "App",
+  components: {
+    HeaderComp,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+body {
+  background: #232323;
+  margin: 0;
+  font-family: "Montserrat", "Avenir";
+}
+
+</style>
